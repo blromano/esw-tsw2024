209d98adc184d48d0efcb275be46d9fa9ff6c867
Victor Ramos <victorrms03@gmail.com>
2024-12-06 00:32:52 -0300

    test: testes preliminares das classes DAO
    Co-Authored-By: Vinícius Ramos <127810265+ViniciusRamoos@users.noreply.github.com>

---
+++ PRJ05 Trash Track/TrashTrack/test/TestesDAO/TesteAdministradoresDAO.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TestesDAO;
+
+import trashtrack.dao.AdministradoresDAO;
+import trashtrack.entidades.Administrador;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author victo
+ */
+public class TesteAdministradoresDAO {
+    
+    public static void main(String[] args) {
+        
+        Administrador administrador = new Administrador();
+        administrador.setId(3);
+        administrador.setEmail("emailEmail");
+        administrador.setSenha("senha");
+        
+        AdministradoresDAO dao = null;
+        
+        try {
+            
+            dao = new AdministradoresDAO();
+            
+            dao.excluir(administrador);
+            
+            
+        } catch (SQLException exc) {
+            
+        }
+        
+    }
+    
+}
